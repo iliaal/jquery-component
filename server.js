f27be4050963881fc78378857efb1fdf8fa47f13
Peter MacMillan <peter@macmillan.io>
2014-05-11 19:05:44 -0400

    simple jade webserver

---
+++ server.js
@@ -0,0 +1,21 @@
+
+var http = require('http')
+  , express = require('express')
+  , app = express()
+  , server = http.createServer(app);
+
+app.set('view engine', 'jade');
+
+app.get(/^\/(.*)/, function (req, res) {
+  if ('' === req.params[0]) {
+    res.redirect(303, '/step0');
+    return;
+  }
+
+  res.render(req.params[0]);
+});
+
+server.listen(process.env.PORT || 3000, function () {
+  console.log('listening on', process.env.PORT || 3000);
+});
+
