d58270841757e3196827df1591bcad9bffb98bae
Peter MacMillan <peter@macmillan.io>
2014-05-11 19:10:42 -0400

    make output pretty and ignore favicon

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ var http = require('http')
 
 app.set('view engine', 'jade');
 
+app.locals.pretty = true;
+
+app.get('/favicon.ico', function (req, res) {
+  res.end(0);
+});
+
 app.get(/^\/(.*)/, function (req, res) {
   if ('' === req.params[0]) {
     res.redirect(303, '/step0');
